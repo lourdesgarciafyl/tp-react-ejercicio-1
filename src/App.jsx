caae4b3f92b7a0fd7cb76d7ddea142ccbebb7160
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-26 12:19:50 -0300

    se agrega el componente Titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Titulo from './components/Titulo';
 
 function App() {
 
   return (
     <>
-
+    <Titulo></Titulo>
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+const Titulo = () => {
+    return(
+        <>
+        <section>
+            <h1 className="text-center display-3">Hello world!</h1>
+        </section>
+        </>
+    );
+}
+
+export default Titulo;
